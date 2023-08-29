8ba95e66c7bccc14fce01de5a36d3baa046a0562
Nirav Parekh <niruparekh09@gmail.com>
2023-08-29 14:58:03 +0530

    31. Next Permutation attempted

--- Data Structures/Array/ArrayPractice.java
+++ Data Structures/Array/ArrayPractice.java
@@ -282,9 +282,47 @@ public class ArrayPractice {
         }
     }
 
+    // 31. Next Permutation
+    public static void nextPermutation(int[] nums) {
+        int sum = 0, n=nums.length;
+        int number = arrayToNumber(nums);
+        sum = digitSum(n, number);
+        boolean flag = true;
+        while(flag){
+            number++;
+            if(digitSum(n, number) == sum){
+                for(int i=n-1;i>=0;i--){
+                    nums[i] = number%10;
+                    number = number/10;
+                }
+                flag =false;
+            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(nums[i]+", ");
+        }
+    }
+
+    public static int arrayToNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum * 10 + nums[i];
+        }
+        return sum;
+    }
+
+    public static int digitSum(int arrayLength, int number){
+        int digitSum =0;
+        for(int i=0; i<arrayLength;i++){
+            digitSum += number%10;
+            number = number/10;
+        }
+        return digitSum;
+    }
+
     public static void main(String args[]) {
-        int[] nums = { 0, 1, 0, 3, 12 };
-        moveZeroes(nums);
+        int[] nums = { 1,1,5 };
+        nextPermutation(nums);
         // for (int i = 0; i < arr.length; i++) {
         // System.out.print(arr[i] + " ");
         // }
