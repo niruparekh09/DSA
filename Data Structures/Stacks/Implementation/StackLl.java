aff1a476bf5b28983a80c9d8f80f1f2ea9b4dc2b
Nirav Parekh <niruparekh09@gmail.com>
2023-11-11 18:40:31 +0530

    new commit

--- Data Structures/Stacks/Implementation/StackLl.java
+++ Data Structures/Stacks/Implementation/StackLl.java
@@ -1,3 +1,57 @@
 public class StackLl {
+    static class Node {
+        int data;
+        Node next;
 
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Stack {
+        static Node head = null;
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        public static void push(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop(){
+            if(isEmpty()){
+                System.out.println("Stack Empty!!!");
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Stack Empty!!!");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Stack list = new Stack();
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+        Stack.push(4);
+        while(!Stack.isEmpty()){
+            System.out.println(Stack.peek());
+            Stack.pop();
+        }
+        System.out.println(Stack.pop());
+    }
 }
