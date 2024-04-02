9688a4a0246e0c443fc12c5149bba44fb3369148
Nirav Parekh <niruparekh09@gmail.com>
2024-04-02 11:54:46 +0530

    Commit

---
+++ Data Structures/SearchingNSorting/Insertion_Sort.java
@@ -0,0 +1,25 @@
+package Searching_and_Sorting;
+
+import java.util.Arrays;
+
+public class Insertion_Sort {
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 2, 7, 1, 7};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
